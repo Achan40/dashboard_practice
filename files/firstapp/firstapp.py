802857ff867a975b16bd8db33997c1a79856ef2e
Aaron <chanman2841@gmail.com>
2021-01-17 16:23:17 -0600

    completed callback fcitons

--- files/firstapp/firstapp.py
+++ files/firstapp/firstapp.py
@@ -14,9 +14,6 @@ app = dash.Dash()
 # items in dataset for region column
 region_options = [{'label': i, 'value': i} for i in happiness['region'].unique()]
 
-# items in dataset for country column
-country_options = [{'label': i, 'value': i} for i in happiness['country'].unique()]
-
 # Multiple inputs
 data_options = [{'label':'Happiness Score','value':'happiness_score'},{'label':'Happiness Rank','value':'happiness_rank'}]
 
@@ -28,14 +25,27 @@ app.layout = html.Div([
         html.A('World Happiness Report Data Source',
             href='https://worldhappiness.report/',
             target='_blank')]),
-    dcc.RadioItems(options=region_options, value='North America'),
-    dcc.Checklist(options=region_options, value=['North America']),
-    dcc.Dropdown(id='country-dropdown',options=country_options, value='United States'),
+    dcc.RadioItems(id='region-radio', options=region_options, value='North America'),
+    dcc.Dropdown(id='country-dropdown'),
     dcc.RadioItems(id='data-radio',options=data_options,value='happiness_score'),
     dcc.Graph(id='happiness-graph'),
     html.Div(id='average-div')
 ])
 
+# country dropdown value depending on region selected
+@app.callback(
+    Output('country-dropdown','options'),
+    Output('country-dropdown','value'),
+    Input('region-radio','value'),
+)
+
+def update_dropdown(selected_region):
+    filtered_happiness = happiness[happiness['region']==selected_region]
+    # get all of the unique countries based on the filtered_happiness dataset which will get the unique countries within the selected regions only
+    country_options = [{'label':i,'value':i} for i in filtered_happiness['country'].unique()]
+    return country_options, country_options[0]['value']
+
+# This call back changes graph depending on selected as well as output avg values for selected country and data type
 # we can omit component_id and component_property if we want
 @app.callback(
     Output(component_id='happiness-graph',component_property='figure'),
