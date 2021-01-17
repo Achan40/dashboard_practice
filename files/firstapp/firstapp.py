2235b6564de39bbb55aaa4b6f17a0c9d4bf3f027
Aaron <chanman2841@gmail.com>
2021-01-17 15:56:12 -0600

    added callbacks to dashboard

--- files/firstapp/firstapp.py
+++ files/firstapp/firstapp.py
@@ -1,6 +1,7 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
 
@@ -16,8 +17,8 @@ region_options = [{'label': i, 'value': i} for i in happiness['region'].unique()
 # items in dataset for country column
 country_options = [{'label': i, 'value': i} for i in happiness['country'].unique()]
 
-# line plot for Happiness Score of US only
-line_fig = px.line(happiness[happiness['country']=='United States'], x='year', y='happiness_score', title='Happiness Score in the USA')
+# Multiple inputs
+data_options = [{'label':'Happiness Score','value':'happiness_score'},{'label':'Happiness Rank','value':'happiness_rank'}]
 
 # rendering dashboard
 app.layout = html.Div([
@@ -29,9 +30,25 @@ app.layout = html.Div([
             target='_blank')]),
     dcc.RadioItems(options=region_options, value='North America'),
     dcc.Checklist(options=region_options, value=['North America']),
-    dcc.Dropdown(options=country_options, value='United States'),
-    dcc.Graph(figure=line_fig)
+    dcc.Dropdown(id='country-dropdown',options=country_options, value='United States'),
+    dcc.RadioItems(id='data-radio',options=data_options,value='happiness_score'),
+    dcc.Graph(id='happiness-graph'),
+    html.Div(id='average-div')
 ])
+
+# we can omit component_id and component_property if we want
+@app.callback(
+    Output(component_id='happiness-graph',component_property='figure'),
+    Output('average-div','children'),
+    Input(component_id='country-dropdown',component_property='value'),
+    Input('data-radio','value')
+)
+
+def update_graph(selected_country, selected_data):
+    filtered_happiness = happiness[happiness['country']==selected_country]
+    line_fig = px.line(filtered_happiness,x='year', y=selected_data,title=f'{selected_data} in {selected_country}')
+    selected_avg = filtered_happiness[selected_data].mean()
+    return line_fig, f'The average {selected_data} for {selected_country} is {selected_avg}'
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ files/secondapp/secondapp.py
@@ -0,0 +1,24 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+
+app = dash.Dash()
+
+app.layout = html.Div([
+    dcc.Input(id='input-text', value='Change this text', type='text'),
+    html.Div(id='output-text')
+])
+
+@app.callback(
+    Output(component_id='output-text', component_property='children'),
+    Input(component_id='input-text', component_property='value')
+)
+
+# The arguement for the fuction is taken from the Input property in the decorator about
+def update_output_div(input_text):
+    return f'Text: {input_text}'
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
